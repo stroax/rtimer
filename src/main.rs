d42666fa16193de531f0ec11e69d9db1c4d63bea
stroax <robertcoulombe@proton.me>
2025-11-01 03:30:52 -0400

    Add function : Enter time

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use std::io;
+use std::env;
 use std::io::{Write, BufReader}; 
 use std::{thread, time, fs::File};
 use notify_rust::Notification;
 use rodio; //::{Decoder, OutputStream, source::Source};
 
 fn countdown(min: i32, sec: i32 ) {
-    println!("min = {}, sec = {}", min, sec);
-
     let mut total_seconds = min * 60 + sec;
     //let second = time::Duration::from_millis(1000);
     let second = time::Duration::from_secs(1);
 
-
-    println!("Total en secondes : {}", total_seconds);
     
-    print!("\x1B[?25l");
+    print!("\x1B[?25l"); // Hide cursor
     while total_seconds > 0 {
         let minutes = total_seconds / 60;
         let seconds = total_seconds % 60;
@@ -24,41 +21,27 @@ fn countdown(min: i32, sec: i32 ) {
         thread::sleep(second);
     }
     println!("00:00");
-    print!("\x1B[?25h");
+    print!("\x1B[?25h"); // Show cursor
     
     Notification::new()
-        .summary("Minuteur")
-        .body("Le temps du minuteur est écoulé")
-        .icon("firefox")
+        .summary("Rtimer")
+        .body("Time is up!")
         //.timeout(Timeout::Milliseconds(10000)) 
         .show().unwrap();
 }
 
 
 fn main() {
-    //let x = 5;
-    
-    println!("1) 03:30");
-    println!("2) 06:00");
-    println!("3) 15:00");
-    println!("4) 35:00");
-    println!("5) 00:10");
+    let args: Vec<String> = env::args().collect();
 
-    let mut input = String::new();
-    print!("Entrez le numéro du minuteur à utiliser : ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).unwrap();
-    let trim_input = input.trim(); // Produit un &str sans retour à la ligne
+    let time = &args[1].clone();
+    let min = &time[0..2];
+    let sec = &time[3..5];
     
-    match trim_input {
-        "1" => countdown(3, 30),
-        "2" => countdown(6, 0),
-        "3" => countdown(15, 0),
-        "4" => countdown(35, 0),
-        "5" => countdown(0, 10),
-        _ => println!("Entrée non valide!"),
-       
-    }
+    let min_int: i32 = min.parse().unwrap();
+    let sec_int: i32 = sec.parse().unwrap();
+
+    countdown(min_int, sec_int); 
 
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()
         .expect("open default audio stream");
